b44bc973bb23fec6f38a43d50e3c058adb6ce396
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-02-17 10:45:49 -0800

    added About, List and Details pages

---
+++ pokedex2.0/src/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Details() {
+  return (
+    <div className="Details">
+      <h1>Details Page</h1>
+    </div>
+  );
+}
+
+export default Details;
---
+++ pokedex2.0/src/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.js';
+
+function List() {
+  return (
+    <div className="List">
+      <h1>List Page</h1>
+    </div>
+  );
+}
+
+export default List;
