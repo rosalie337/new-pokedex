7a3a2b1122367f3ceba44a819ad38937e5802a3c
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-02-17 11:00:41 -0800

    maade changes to App.js

--- pokedex2.0/src/App.js
+++ pokedex2.0/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { 
+  Route, 
+  Switch,
+  Link,
+  BrowserRouter as Router, 
+} from 'react-router-dom';
+import Detail from './Detail';
+import Home from './Home';
+import About from './About.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App Page</h1>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+      return (
+        <Router>
+          <div>
+            <h2>Pokemon search</h2>
+            <Link to="/">Home</Link>
+            <Link to="/about-me/some-other-param">About the Developer</Link>
+            <Switch>
+              <Route exact path="?:name?" component={Home} />
+              <Route exact path="/about-me/:other/:thing/:here" component={About} />
+              <Route exact path="/characters/:charId" component={Detail} />
+            </Switch>
+          </div>
+        </Router>
+      );
+    }
 }
-
-export default App;
